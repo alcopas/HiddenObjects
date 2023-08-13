df502839c8e914dcbf30d2de4cd7b987f56e7da7
Douglas Copas <douglas.copas@gmail.com>
2023-08-13 16:33:40 +0200

    slides

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ from kivy.clock import Clock
 from kivy.uix.button import Button
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+from kivy.cache import Cache
+Cache.remove('slide1.jpg')
+Cache.remove('slide2.jpg')
+Cache.remove('slide3.jpg')
 
 DEBUG = False
 
