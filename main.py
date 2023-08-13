fc3acb47b052d790aab543da6a8037c294f9be81
Douglas Copas <douglas.copas@gmail.com>
2023-08-13 21:46:37 +0200

    try ogg

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ class IntroScreen(Screen):
         super().__init__(**kwargs)
         print("IntroScreen initialized") if DEBUG else None
 
-    music = SoundLoader.load('intro_music.mp3')
+    music = SoundLoader.load('intro_music.ogg')
 
     def on_enter(self):
         self.music.play()
